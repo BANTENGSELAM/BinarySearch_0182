3eb9035ae7d7ffe14fbfb982a9c50e90dbbc8107
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-04-17 14:40:49 +0700

    Void Input

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -5,3 +5,20 @@ int element[10];
 int nPanjang;
 int x; //library
 
+void input () // input
+{
+    while (true)
+    {
+        cout << "Masukkan  Banyaknya Elemen Pada Array (Maksimal 10) : ";
+        cin >> nPanjang;
+        
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout<<"\n[!] Jumlah Elemen Tidak Boleh Lebih Dari 10. Silahkan Coba Lagi.\n";
+        }
+    }
+}
